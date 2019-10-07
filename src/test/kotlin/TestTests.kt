1b7b1bd858ed16344c0289ed2b269601d1b032ce
Piotr Rusin <piotr.rusin88@gmail.com>
2019-10-08 00:17:46 +0200

    Add test dependencies and an example test
    JUnit5 and AssertK are being added.

---
+++ src/test/kotlin/TestTests.kt
@@ -0,0 +1,10 @@
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import org.junit.jupiter.api.Test
+
+class TestTests {
+    @Test
+    internal fun `example test`() {
+        assertThat("a").isEqualTo("a")
+    }
+}
