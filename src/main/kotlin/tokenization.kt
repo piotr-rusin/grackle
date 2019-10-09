e3a15c80c9e7678eafefdb087023ef6e54a87e47
Piotr Rusin <piotr.rusin88@gmail.com>
2019-10-09 22:12:00 +0200

    Override equals and hashCode in Token class

--- src/main/kotlin/tokenization.kt
+++ src/main/kotlin/tokenization.kt
@@ -17,4 +17,22 @@ class Token(val value: String, val tokenType: TokenType) {
     override fun toString(): String {
         return "Token(value='$value', tokenType=$tokenType)"
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Token
+
+        if (value != other.value) return false
+        if (tokenType != other.tokenType) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = value.hashCode()
+        result = 31 * result + tokenType.hashCode()
+        return result
+    }
 }
